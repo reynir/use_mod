2826fd9dfc1ab550a1e768d053a4fd03e1b5cfe9
Reynir Björnsson <reynir@reynir.dk>
2018-09-24 11:56:09 +0200

    Don't forget to print a newline!

--- use_mod.ml
+++ use_mod.ml
@@ -3,4 +3,4 @@ let () =
     Toploop.directive_table
     "use_mod"
     (Toploop.Directive_string (fun path ->
-      Printf.printf "Use #mod_use %S;; instead" path))
+      Printf.printf "Use #mod_use %S;; instead\n" path))
